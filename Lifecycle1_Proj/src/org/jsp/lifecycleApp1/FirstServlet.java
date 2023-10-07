6f02bdb3ab9e504db9bc8b8788611e82f9d8801b
abhiverma2001 <135794822+abhiverma2001@users.noreply.github.com>
2023-10-07 12:06:41 +0530

    servlet lifecycle project using Annotation

---
+++ Lifecycle1_Proj/src/org/jsp/lifecycleApp1/FirstServlet.java
@@ -0,0 +1,34 @@
+package org.jsp.lifecycleApp1;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+@WebServlet("/fs")
+public class FirstServlet extends GenericServlet
+{
+	public FirstServlet() 
+	{
+		System.out.println("Servlet object is created");
+	}
+	@Override
+	public void init(ServletConfig config) throws ServletException{
+		System.out.println("Servlet Object is initialized");
+	}
+@Override
+	public void service(ServletRequest req, ServletResponse resp) throws ServletException, IOException {
+		
+
+	String name=req.getParameter("nm");
+	String place=req.getParameter("pl");
+	PrintWriter out=resp.getWriter();
+	out.println("<html><body bgcolor='yellow'>"
+			+ "<h1>Student name is "+name+" from "
+					+place+ "</h1>" +"</body></html>");
+	out.close();
+	System.out.println("service method is executed");
+	}
+ @Override
+ public void destroy() {
+	 System.out.println("Closed all the costly resources!!");
+ }
+}
+
