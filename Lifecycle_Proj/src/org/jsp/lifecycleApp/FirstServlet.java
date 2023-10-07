04acb6f8f7217010c336eb4c0f6feb83a37cc4cf
abhiverma2001 <135794822+abhiverma2001@users.noreply.github.com>
2023-10-07 11:57:11 +0530

    servlet lifecycle using web.xml

---
+++ Lifecycle_Proj/src/org/jsp/lifecycleApp/FirstServlet.java
@@ -0,0 +1,30 @@
+package org.jsp.lifecycleApp;
+import java.io.*;
+import javax.servlet.*;
+public class FirstServlet extends GenericServlet{
+	public FirstServlet() {
+		System.out.println("Servlet object is created");
+	}
+	@Override
+	public void init(ServletConfig config) throws ServletException{
+		System.out.println("Servlet Object is initialized");
+	}
+@Override
+	public void service(ServletRequest req, ServletResponse resp) throws ServletException, IOException {
+		
+
+	String name=req.getParameter("nm");
+	String place=req.getParameter("pl");
+	PrintWriter out=resp.getWriter();
+	out.println("<html><body bgcolor='yellow'>"
+			+ "<h1>Student name is "+name+" from "
+					+place+ "</h1>" +"</body></html>");
+	out.close();
+	System.out.println("service method is executed");
+	}
+ @Override
+ public void destroy() {
+	 System.out.println("Closed all the costly resources!!");
+ }
+
+}
