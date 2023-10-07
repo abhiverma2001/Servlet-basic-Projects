7c49fbf780f2810a4837077ce33ccb0cc51040e3
abhiverma2001 <135794822+abhiverma2001@users.noreply.github.com>
2023-10-07 11:16:50 +0530

    basic project on UI Form data using web.xml

---
+++ Ui_Proj/src/org/jsp/uiApp/FirstServlet.java
@@ -0,0 +1,18 @@
+package org.jsp.uiApp;
+import java.io.*;
+import javax.servlet.*;;
+public class FirstServlet extends GenericServlet {
+@Override
+	public void service(ServletRequest req, ServletResponse resp) throws ServletException, IOException 
+{
+		
+		String name=req.getParameter("nm");
+		String place=req.getParameter("pl");
+		PrintWriter out=resp.getWriter();
+		out.println("<html><body bgcolor='yellow'>"
+				+ "<h1>Student name is "+name+" from "
+						+place+ "</h1>" +"</body></html>");
+		out.close();
+	}
+   
+}
